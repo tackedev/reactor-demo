a841d99f934ba25ee7f76eba9f4700c119f9ba3d
tackedev <quangky.it@gmail.com>
2022-08-09 23:59:42 +0700

    add Mono api example

---
+++ src/main/java/org/tackedev/reactor/Common.java
@@ -0,0 +1,23 @@
+package org.tackedev.reactor;
+
+import com.github.javafaker.Faker;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Common {
+    private static final Faker FAKER = Faker.instance();
+
+    public static String getName() {
+        return FAKER.name().fullName();
+    }
+
+    public static void sleepInSeconds(long seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/tackedev/reactor/mono/MonoExample.java
@@ -0,0 +1,55 @@
+package org.tackedev.reactor.mono;
+
+import org.tackedev.reactor.Common;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+
+public class MonoExample {
+
+    public static void main(String[] args) {
+
+        Mono.just(1).subscribe(i -> System.out.println("Received: " + i));
+
+        Mono.fromSupplier(Common::getName)
+                .subscribe(s -> System.out.println("Received: " + s));
+
+        Mono.fromSupplier(MonoExample::getNameInLongTime)
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(s -> System.out.println("Received: " + s));
+
+        String name = Mono.fromSupplier(MonoExample::getNameInLongTime)
+                .subscribeOn(Schedulers.boundedElastic())
+                .block();
+        System.out.println("Received: " + name);
+
+        Mono.fromFuture(getNameFuture())
+                .subscribe(s -> System.out.println("Received: " + s));
+        Common.sleepInSeconds(1);
+
+        Mono.fromCallable(getNameCallable())
+                .subscribe(
+                        s -> System.out.println("Received: " + s),
+                        Throwable::printStackTrace,
+                        () -> System.out.println("Runnable completed!")
+                );
+    }
+
+    public static Callable<String> getNameCallable() {
+        return MonoExample::getNameInLongTime;
+    }
+
+    public static CompletableFuture<String> getNameFuture() {
+        return CompletableFuture.supplyAsync(Common::getName);
+    }
+
+    public static String getNameInLongTime() {
+        System.out.println("Generating name...");
+        Common.sleepInSeconds(3);
+        return Common.getName();
+    }
+
+
+}
